c8a0765c3b219e7461e99363d4f626ed5c224329
EricBenitez01 <leonetkd@gmail.com>
2023-12-19 21:31:43 -0300

    se resuelve error

--- bar_point_backend/src/controllers/dashboardController.js
+++ bar_point_backend/src/controllers/dashboardController.js
@@ -2,56 +2,78 @@ const db = require('../database/models');
 const bcrypt = require('bcrypt');
 const path = require("path");
 const fs = require('fs').promises;
- const { Op } = require('sequelize');
+const {
+    Op
+} = require('sequelize');
 
 module.exports = {
 
     status: async (req, res) => {
-      try {
-        let businessId = req.params.id;
-    
-        const currentDate = new Date();
-        const lastMonth =  currentDate.setMonth(currentDate.getMonth() - 1);
-        const lastWeek =  currentDate.setMonth(currentDate.getDay() - 7);
-    
-        const userCount = await db.User_points.count({
-          where: {
-            businessfk: businessId,
-          },
-        });
+        try {
+            let businessId = req.params.id;
 
-        const userPointsTotal = await db.User_points.sum('quantity', {
-          where: {
-            businessfk: businessId,
-            create_time: {
-              [Op.gte]: lastMonth,
-            },
-          },
-        });
+            const currentDate = new Date();
 
-        const userPointsLastWeek = await db.User_points.sum('quantity', {
-            where: {
-              businessfk: businessId,
-              create_time: {
-                [Op.gte]: lastWeek,
-              },
-            },
-        });
+            const lastMonth = new Date();
+            lastMonth.setMonth(lastMonth.getMonth() - 1);
 
-        const benefitCreated = await db.Benefit.count({
-            where: {businessfk: businessId}
-        });
-    
-        res.status(200).json({
-          users: userCount,
-          userPointsLastMonth: userPointsTotal,
-          userPointsLastWeek: userPointsLastWeek,
-          benefitCreated: benefitCreated
-        });
-      } catch (error) {
-        console.error('Error al obtener los puntos del último mes:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-      }
+            const lastWeek = new Date();
+            lastWeek.setDate(lastWeek.getDate() - 7);
+
+            const timestampValue = currentDate.getTime() / 1000.0;
+
+            const userCount = await db.User_points.count({
+                where: {
+                    businessfk: businessId,
+                },
+            });
+
+            const userPointsSum = await db.User_points.sum('quantity', {
+                where: {
+                    businessfk: businessId,
+                    create_time: {
+                        [Op.gte]: sequelize.fn('to_timestamp', timestampValue),
+                    },
+                },
+            });
+
+            const userPointsLastMonth = await db.User_points.sum('quantity', {
+                where: {
+                    businessfk: businessId,
+                    create_time: {
+                        [Op.gte]: lastMonth,
+                    },
+                },
+            });
+
+            const userPointsLastWeek = await db.User_points.sum('quantity', {
+                where: {
+                    businessfk: businessId,
+                    create_time: {
+                        [Op.gte]: lastWeek,
+                    },
+                },
+            });
+
+            const benefitCreated = await db.Benefit.count({
+                where: {
+                    businessfk: businessId
+                },
+            });
+
+            res.status(200).json({
+                users: userCount,
+                userPointsLastMonth: userPointsLastMonth,
+                userPointsLastWeek: userPointsLastWeek,
+                benefitCreated: benefitCreated,
+            });
+        } catch (error) {
+            console.error('Error al obtener los puntos del último mes:', error);
+            res.status(500).json({
+                error: 'Internal Server Error'
+            });
+        }
     }
+
 
 }
